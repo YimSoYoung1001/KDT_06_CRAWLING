2ad84ec2ff0e9eaea3327fd1ba2f22ad64330150
YimSoYoung <l53314951@gmail.com>
2024-02-13 09:18:27 +0900

    24.02.12.

--- day02_hw/cardgame.py
+++ day02_hw/cardgame.py
@@ -12,14 +12,17 @@ def play_game():
     dealer = GameDealer() #dealer라는 객체 생성
     dealer.make_deck()    #카드 deck 1개를 만듦
     dealer.random_print_deck()  #랜덤하게 섞어서 인출
-    print(' \n\n\n')
+    card_all = 52
+
+    print(' \n')
 
     # [카드게임 : 1단계]
-    print('='*50)
+    print('=' * 50)
     share_num = 10
     print(f'카드 나누어 주기: {share_num}장')
-    print('-'*50)
-    print(f'[GameDealer] 딜러가 가진 카드 수 : {52-share_num-share_num}')
+    print('-' * 50)
+    card_all = card_all - share_num - share_num
+    print(f'[GameDealer] 딜러가 가진 카드 수 : {card_all}')
     player1.add_card_list(dealer.deck[:10])
     dealer.deck = dealer.deck[10:]
     player2.add_card_list(dealer.deck[:10])
@@ -29,18 +32,65 @@ def play_game():
     player1.display_two_card_lists()
     player2.display_two_card_lists()
 
+    print(' \n')
 
-    print(' \n\n\n')
     # [카드게임 : 2단계]
-    answer = ''
-    #answer = input('[2]단계: 다음 단계 진행을 위해 Enter 키를 누르세요!: ')
+    # answer = ''
+    answer = input('[2]단계: 다음 단계 진행을 위해 Enter 키를 누르세요!: ')
     if answer == '':
         player1.check_one_pair_card()
-        print('중복검사 완료')
+        print(f"-{player1.name}의 중복검사 완료-")
         player1.display_two_card_lists()
 
         player2.check_one_pair_card()
+        print(f'-{player2.name}의 중복검사 완료-')
         player2.display_two_card_lists()
+
+    level_count = 3
+    game_end = True
+    while game_end:
+        # [카드게임 : 3단계]
+        answer = input(f'[{level_count}]단계: 다음 단계 진행을 위해 Enter 키를 누르세요!: ')
+        if answer == '':
+            print('='*50)
+            share_num = 4
+            print(f'카드 나누어 주기: {share_num}장')
+            print('-'*50)
+            card_all = card_all - share_num - share_num
+            print(f'[GameDealer] 딜러가 가진 카드 수 : {card_all}')
+            player1.add_card_list(dealer.deck[:10])
+            dealer.deck = dealer.deck[10:]
+            player2.add_card_list(dealer.deck[:10])
+            dealer.deck = dealer.deck[10:]
+            print()
+
+            player1.display_two_card_lists()
+            player2.display_two_card_lists()
+            print(' \n')
+
+
+            player1.check_one_pair_card()
+            print(f"-{player1.name}의 중복검사 완료-")
+            player1.display_two_card_lists()
+
+            player2.check_one_pair_card()
+            print(f'-{player2.name}의 중복검사 완료-')
+            player2.display_two_card_lists()
+
+
+            # 게임 종료 조건
+            if ((len(dealer.deck) == 0) |
+                (len(player1.holding_card_list) == 0) |
+                (len(player2.holding_card_list) == 0)):
+                print(f"딜러가 가진 카드의 수 : {len(dealer.deck)}")
+                print(f"{player1.name}이 가진 카드의 수 : {len(player1.holding_card_list)}")
+                print(f"{player2.name}이 가진 카드의 수 : {len(player2.holding_card_list)}")
+                print('   게   임   종   료   ')
+                game_end = False
+
+            level_count += 1
+
+
 
 
 
--- day02_hw/player.py
+++ day02_hw/player.py
@@ -2,6 +2,7 @@
 # 자신이 가지고 있는 카드 관리 (2개의 리스트 가짐 : holding_card_list, open_card_list)
 # 두 장의 동일한 카드를 제거하는 기능 (번호가 동일한 경우, holding_card_list에서 open_card_list로 이동 => 테이블에 공개하는 기능)
 
+
 class Player:
     def __init__(self, name):
         self.name = name
@@ -14,6 +15,8 @@ class Player:
     def display_two_card_lists(self):
         print('=' * 50)
         print(f"[{self.name}] Open card list: {len(self.open_card_list)}")
+        for card in self.open_card_list:
+            print(card, end=' ')
         print()
         print(f"[{self.name}] Holding card list: {len(self.holding_card_list)}")
         for card in self.holding_card_list:
@@ -28,25 +31,33 @@ class Player:
 
         for i in range(length-1):
             one_card = self.holding_card_list[i]
-
             for k in range(i+1, length):
                 second_card = self.holding_card_list[k]
 
+                #중복카드라고 판단되면 open_card_list에 들어감
                 if one_card.number == second_card.number:
                     if one_card not in self.open_card_list:
                         self.open_card_list.append(one_card)
-
                     if second_card not in self.open_card_list:
                         self.open_card_list.append(second_card)
 
+        #중복카드가 모두 open_card_list에 들어갔으니 그 카드들을 holding_card_list에서 제거해야함
+        for i in range(10):           #이렇게 여러번 돌리니 open + holding의 개수가 맞음 (이유는 모르겠음)
             for card in self.holding_card_list:
                 if card in self.open_card_list:
+                    #print('중복카드 존재 =', card)
                     self.holding_card_list.remove(card)
 
 
 
+
+
+
+
+
+
 # ----------------------------------------------------------------------------------------------------------------
-# 시행착오
+# ETC (시행착오)
 # ----------------------------------------------------------------------------------------------------------------
 
     """
